6ad75234568fc36d7eb0d9dbdd5a436bf4b0cfcb
Flávio Coutinho <fegemo@gmail.com>
2021-08-22 16:41:06 -0300

    Adds loop to fit/predict, but...
    Accuracy is still low.
    Investigating error update.

--- couto_boost.py
+++ couto_boost.py
@@ -7,7 +7,7 @@ class CoutoBoostClassifier:
     interface pública dos classificadores
     do sk-learn.
     """
-    def __init__(self, max_estimators=27):
+    def __init__(self, max_estimators=108):
         self.max_estimators = max_estimators
         
     
@@ -36,7 +36,8 @@ class CoutoBoostClassifier:
         
         # Em alto nível:
         # 1. cria stumps (seguindo uma estratégia específica para tic-tac-toe)
-        stumps = _create_stumps(X)
+        self.stumps = self._create_stumps(X)
+        print("stumps totais: ", len(self.stumps))
         
         # 2. inicializa escolhas das iterações
         self.iteration_errors = []
@@ -44,20 +45,31 @@ class CoutoBoostClassifier:
         self.iteration_stumps = []
         
         # 3. cria pesos dos exemplos
-        self.input_weights = np.full((1,X.shape[0]), 1/X.shape[0])
-        
+        samples = X.shape[0]
+        print("samples = ", samples)
+        self.input_weights = np.full(samples, 1/samples)
+        print("Peso inicial = ", self.input_weights[0])
+
         # 4. repete
-        for i in range(self.n_estimators):
+        for t in range(self.max_estimators):
+            print("Iteração t=", t)
+            print("iteration_stumps: ", self.iteration_stumps)
+            print("iteration_errors: ", self.iteration_errors)
+            print("iteration_alphas: ", self.iteration_alphas)
             # 4.1. seleciona melhor stump (e o remove da lista de disponíveis)
-            best_stump, error, mistakes = _pick_best_stump(X, y)
+            best_stump, best_stump_id, error, mistakes = self._pick_best_stump(X, y)
+            print("best_stump_index: ", best_stump_id)
+            print("best_stump: ", best_stump)
+            print("error: ", error)
+            print("mistakes: ", len(mistakes))
             
             # 4.2. calcula importância do stump selecionado
             # 4.3. atualiza os pesos considerando esse stump
-            _update_weights(X, error, mistakes)
+            self._update_weights(error, mistakes)
             
             # 4.4. se erro = 0 ou selecionou max_estimators, sai
             if error == 0:
-                print("Saiu mais cedo porque erro empírico zerou na iteração " + str(i))
+                print("Saiu mais cedo porque erro empírico zerou na iteração " + str(t))
                 break
 
         
@@ -85,11 +97,13 @@ class CoutoBoostClassifier:
         # 2. multiplica o resultado de cada um pelo alfa
         # 3. soma tudo e retorna
         value = 0
-        classified = np.ndarray(shape=(X.shape, 1))
+        classified = np.zeros((X.shape[0], 1))
         for i in range(X.shape[0]):
             
             classified[i] = 0
-            for t, (feature, value, label) in enumerate(self.iteration_stumps):
+            for t, stump_id in enumerate(self.iteration_stumps):
+                (feature, value, label) = self.stumps[stump_id]
+
                 alpha = self.iteration_alphas[t]
                 
                 if X[i, feature] == value:
@@ -119,20 +133,21 @@ class CoutoBoostClassifier:
                 # cada valor de positivo ou negativo
                 for label in [-1, 1]:
                     stumps.append((feature, value, label))
+        print("all_stumps: ", len(stumps))
         return stumps
 
     
     def _create_stumps(self, X):
-        all_stumps = _create_all_posible_stumps(X)
+        all_stumps = np.array(self._create_all_posible_stumps(X))
         stumps = []
         
         # vamos precisar selecionar alguns stumps
         if self.max_estimators < len(all_stumps):
-            stumps = np.random.choice(all_stumps, self.max_estimators, replace=False)
+            chosen_stump_indices = np.random.choice(len(all_stumps), size=self.max_estimators, replace=False)
+            stumps = all_stumps[chosen_stump_indices].tolist()
         # pediu mais stumps do que tem como criar (sem redundância)
-        else if self.max_estimators > len(all_stumps):
-            print("Foi solicitada uma quantidade de estimadores maior do que possível")
-            max_estimators = len(all_stumps)
+        elif self.max_estimators > len(all_stumps):
+            # print("Foi solicitada uma quantidade de estimadores maior do que possível")
             stumps = all_stumps
         # quantidade de stumps solicitada é igual ao máximo
         else:
@@ -147,42 +162,58 @@ class CoutoBoostClassifier:
         
         best_stump_id = -1
         best_stump_error = float("inf")
+        temp_stump_errors = []
         mistaken_sample_ids = []
-        
+
         # percorre stumps e pega o que menos errou
         for stump_id, (feature, value, label) in enumerate(self.stumps):
+            # print("Avaliando stump " + str(stump_id) + ": " + str(self.stumps[stump_id]))
             stump_error = 0
-            
+            mistaken_sample_ids = []
+
             # não colocar o mesmo stump mais de uma vez
-            if stump_id in self.iteration_stumps:
-                continue
+            # if stump_id in self.iteration_stumps:
+            #     print(f"Saltou o {stump_id} pq já foi usado: {self.iteration_stumps}")
+            #     continue
             
             # percorre os exemplos verificando se este stump errou cada um
-            for sample_id, x fom enumerate(X):
+            # print("Percorrendo exemplos")
+            print("X:", X.shape)
+            for sample_id, x in enumerate(X):
                 # stump e exemplo têm mesma feature?
                 feature_matches = x[feature] == value
                 # stump e exemplo têm mesmo rótulo?
                 label_matches = y[sample_id] == label
-                
+                # print("Para exemplo " + str(sample_id) + ": ", X[sample_id])
+                # print("feature_matches: ", feature_matches)
+                # print("label_matches: ", label_matches)
+
                 if (feature_matches and not label_matches) or (not feature_matches and label_matches):
+                    # print("Somando erro de " + str(self.input_weights[sample_id]) + " ao stump " + str(stump_id))
                     stump_error += self.input_weights[sample_id]
                     mistaken_sample_ids.append(sample_id)
             
+            temp_stump_errors.append({ "id": stump_id, "error": stump_error})
+
             if stump_error < best_stump_error:
                 best_stump_error = stump_error
                 best_stump_id = stump_id
         
+        if best_stump_id == -1:
+            print("temp_stump_errors", temp_stump_errors)
+            raise Exception("Nenhum stump foi escolhido")
 
-        self.iteration_stumps.append(best_stump)
-        self.iteration_errors.append(error)
+        self.iteration_stumps.append(best_stump_id)
+        self.iteration_errors.append(best_stump_error)
         
-        return self.stumps[best_stump_id], best_stump_error, mistaken_sample_ids
+        return self.stumps[best_stump_id], best_stump_id, best_stump_error, mistaken_sample_ids
     
     
-    def _update_weights(self, X, error, mistaken_sample_ids):
+    def _update_weights(self, error, mistaken_sample_ids):
         
         alpha = np.log((1-error)/error) / 2
         self.iteration_alphas.append(alpha)
+        print(f"Errou {len(mistaken_sample_ids)} exemplos")
         
         # acha novos pesos (sem normalizar ainda)
         weight_sum = 0
@@ -193,10 +224,11 @@ class CoutoBoostClassifier:
                 multiplier = -1
                 
             new_weight = weight * np.exp(-alpha * multiplier)
+            self.input_weights[sample_i] = new_weight
             weight_sum += new_weight
             
             
         # normaliza (divide por z)
-        self.input_weights = map(self.input_weights, lambda: w => w/weight_sum)
+        self.input_weights = np.divide(self.input_weights, weight_sum)
 
         
